f83c85d60d06d8887f4db088b4c51de0fb2c5fd7
hmilly <milstowers@gmail.com>
2023-05-22 20:54:48 +0100

    header reconfigured/updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,77 +17,73 @@ const Header = () => {
   };
 
   return (
-    <header className="navbar flex-fill fixed-top bg-dark flex-sm-row flex-column py-1">
-      <Link
-        to="/"
-        className="navbar-brand text-center text-light m-0 p-0 flex-fill"
-      >
-        <h1 className="p-0 m-0">Artsy</h1>
-      </Link>
-      <Nav
-        activeKey="/home"
-        as="ul"
-        className="flex-sm-row flex-column gap-2 flex-fill justify-content-evenly"
-      >
-        <Row className="row-cols-1 row-cols-sm-2 flex-sm-row flex-fill">
-          <Col className="d-flex justify-content-evenly p-1">
+    <header className="fixed-top bg-dark">
+      <Row as="nav" className="navbar row-cols-1 row-cols-sm-3">
+        <Col
+          as="ul"
+          className="d-flex justify-content-evenly p-1 m-0 order-sm-1 order-2"
+        >
+          <Nav.Item as="li">
+            <Link to="/" className="p-1 text-light">
+              <AiFillHome className="img-fluid" />
+            </Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/sellers" className="p-1 text-light">
+              <AiOutlineShop className="img-fluid" />
+            </Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/paintings" className="p-1 text-light">
+              <AiOutlinePicture className="img-fluid" />
+            </Link>
+          </Nav.Item>
+        </Col>
+        <Col className="order-sm-2 order-1">
+          <Link to="/" className="text-center text-light m-0 p-0">
+            <h1 className="p-0 m-0">Artsy</h1>
+          </Link>
+        </Col>
+        {userId !== null ? (
+          // user logged in - show profile btn & log out
+          <Col
+            as="ul"
+            className="d-flex justify-content-evenly p-1 m-0 order-3 align-items-center"
+          >
             <Nav.Item as="li">
-              <Link to="/" className="p-1 text-light">
-                <AiFillHome className="img-fluid" />
+              <Link to="/profile" className="p-1 text-light">
+                <BsFillPersonVcardFill className="img-fluid " />
               </Link>
             </Nav.Item>
             <Nav.Item as="li">
-              <Link to="/sellers" className="p-1 text-light">
-                <AiOutlineShop className="img-fluid" />
+              <button
+                className="btn btn-link link-light"
+                type="button"
+                onClick={logOut}
+              >
+                Log Out
+              </button>
+            </Nav.Item>
+          </Col>
+        ) : (
+          // no user logged in - sign in / up btn
+          <Col
+            as="ul"
+            className="d-flex justify-content-evenly p-1 m-0 order-3"
+          >
+            <Nav.Item as="li">
+              <Link to="/sign-in" className=" p-1 text-light">
+                Sign in
               </Link>
             </Nav.Item>
             <Nav.Item as="li">
-              <Link to="/paintings" className="p-1 text-light">
-                <AiOutlinePicture className="img-fluid" />
+              <Link to="/sign-up" className=" p-1 text-light">
+                Sign Up
               </Link>
             </Nav.Item>
           </Col>
-          {userId !== null ? (
-            // user logged in - show profile btn & log out
-            <Col className="d-flex justify-content-evenly p-1">
-              <Nav.Item as="li">
-                <Link to="/profile" className="p-1 text-light">
-                  <BsFillPersonVcardFill className="img-fluid " />
-                </Link>
-              </Nav.Item>
-              <Nav.Item as="li">
-                <button
-                  className="btn btn-link link-light text-decoration-underline"
-                  type="button"
-                  onClick={logOut}
-                >
-                  Log Out
-                </button>
-              </Nav.Item>
-            </Col>
-          ) : (
-            // no user logged in - sign in / up btn
-            <Col className="d-flex flex-fill justify-content-evenly p-1">
-              <Nav.Item as="li">
-                <Link
-                  to="/sign-in"
-                  className="text-decoration-underline p-1 text-light"
-                >
-                  Sign in
-                </Link>
-              </Nav.Item>
-              <Nav.Item as="li">
-                <Link
-                  to="/sign-up"
-                  className="text-decoration-underline p-1 text-light"
-                >
-                  Sign Up
-                </Link>
-              </Nav.Item>
-            </Col>
-          )}
-        </Row>
-      </Nav>
+        )}
+      </Row>
     </header>
   );
 };
